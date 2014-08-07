4619849d95bee6cbf7f2efe3df15ae7746f95d2d
William Creighton <william.creighton@gmail.com>
2014-08-07 10:40:51 -0400

    Updated user-dashboard-page display

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -51,13 +51,6 @@ var AppRouter = Parse.Router.extend({
 
 	userDashboard: function(){
 		$('.content').html('');
-			// enforceAuthenticated: function(){
-			// 	if ! Parse.User.current(){
-			// 		router.navigate('', {trigger:true});
-			// 	} else {
-			// 		new UserDashboardPage();
-			// 	}
-			// }; 
 		new UserDashboardPage();
 	},
 
